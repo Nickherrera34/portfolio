44f514d0f4fe73e4592b404a87fcc3bfe876357d
Nick Herrera <nickherrera34@gmail.com>
2018-01-28 11:30:42 -0600

    set up server and connected routes

---
+++ routes/html-routes.js
@@ -0,0 +1,10 @@
+// add routes
+var path = require("path");
+var express = require('express');
+var router = express.Router();
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(process.cwd() + '/public'));
+
+// Set Handlebars
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them
+require('./routes/html-routes.js')(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
